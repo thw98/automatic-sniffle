9ee6705792ef2614bc6409742d77e3fe8716d7f2
thw98 <85386051+thw98@users.noreply.github.com>
2021-06-07 18:08:00 +0800

    Add files via upload

---
+++ src/components/movie.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+
+  const moviedetails = (id) => {
+    const url = "https://www.imdb.com/title/" + id
+    window.location.href = url
+  }
+
+const Movie = ({ movie }) => {
+  const poster =
+    movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+  return (
+    <div className="movie">
+      <h2>{movie.Title}</h2>
+      <div>
+        <img
+          width="200"
+          alt={`The movie titled: ${movie.Title}`}
+          src={poster}
+          onClick={() => moviedetails(movie.imdbID)} 
+        />
+      </div>
+      <p>({movie.Year})</p>
+    </div>
+  );
+};
+
+
+export default Movie;
